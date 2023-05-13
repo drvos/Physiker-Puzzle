90a0762769cf26f21594c5a267c1e155d1e18a9f
Volker Schering <mail@volker-schering.de>
2023-05-13 18:51:04 +0200

    Display Swapcount in label
    Data transfer via segue

---
+++ tre fysiker katte/GameInfoTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameInfoTableViewController.swift
+//  tre fysiker katte
+//
+//  Created by Volker Schering on 13.05.23.
+//
+
+import UIKit
+   
+class GameInfoTableViewController: UITableViewController {
+   
+   @IBOutlet weak var lblMoves: UILabel!
+   @IBOutlet weak var lblTime: UILabel!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+}

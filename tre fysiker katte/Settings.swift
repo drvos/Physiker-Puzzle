e3ff2582bf6c3bde5fa700bcef7e202cc7317966
Volker Schering <mail@volker-schering.de>
2023-05-14 10:23:28 +0200

    ConfigurationTableViewController
    SegmentControl for PuzzleLevel

---
+++ tre fysiker katte/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  tre fysiker katte
+//
+//  Created by Volker Schering on 14.05.23.
+//
+
+import Foundation
--- tre fysiker katte/ViewController/ConfigurationTableViewController.swift
+++ tre fysiker katte/ViewController/ConfigurationTableViewController.swift
@@ -9,8 +9,37 @@ import UIKit
 
 class ConfigurationTableViewController: UITableViewController {
  
+   var delegate: configurationTableViewControllerDelegate!
+
+   @IBOutlet weak var puzzleLevelSegment: UISegmentedControl!
+   @IBOutlet weak var puzzleLevelLabel: UILabel!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
    }
    
+   @IBAction func segmentPuzzleLevel(_ sender: UISegmentedControl) {
+      let segmentIndex = Int(puzzleLevelSegment.selectedSegmentIndex)
+      print("SegmentPuzzleLevel: \(segmentIndex)")
+      var puzzleLevel: PuzzleLevel
+      switch (puzzleLevelSegment.selectedSegmentIndex) {
+         case 0: // einfach
+            puzzleLevelLabel.text = "wenig Teile"
+            puzzleLevel = .easy
+         case 1: // normal
+            puzzleLevelLabel.text = "normale Teile"
+            puzzleLevel = .normal
+         case 2: // schwer
+            puzzleLevelLabel.text = "viele Teile"
+            puzzleLevel = .hard
+         default:
+            puzzleLevelLabel.text = "Hö?"
+            puzzleLevel = .veryEasy
+      }
+      delegate?.puzzleLevelChanged(puzzleLevel: puzzleLevel)
+   }
+}
+
+protocol configurationTableViewControllerDelegate {
+   func puzzleLevelChanged(puzzleLevel: PuzzleLevel)
 }

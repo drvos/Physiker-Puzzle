eec8f51b54452c52cbb1e3001002b97b7d1eafc4
Volker Schering <mail@volker-schering.de>
2023-05-18 09:06:22 +0200

    Bundle moved to ViewController

--- Physiker Katzen/ViewController/ViewController.swift
+++ Physiker Katzen/ViewController/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, PuzzleDelegate, SettingsDelegate {
 
+   @IBOutlet weak var appTitle: UINavigationItem!
    @IBOutlet weak var puzzleView: SlidingPuzzleView!
    
    let settings = Settings.shared
@@ -17,9 +18,15 @@ class ViewController: UIViewController, PuzzleDelegate, SettingsDelegate {
    override func viewDidLoad() {
       super.viewDidLoad()
       
+      print(String(format: "Name %@", Bundle.main.appName))
+      print(String(format: "Bundle %@", Bundle.main.bundleId))
+      print(String(format: "Version %@", Bundle.main.versionNumber))
+      print(String(format: "Build %@", Bundle.main.buildNumber))
+      
       puzzleView.delegate = self
       settings.delegate = self
       
+      navigationItem.title = Bundle.main.appName
       navigationItem.rightBarButtonItem =
       UIBarButtonItem(image: UIImage(systemName: "gearshape"), style: .plain,
                       target: self, action: #selector(prefsTapped))
@@ -64,4 +71,26 @@ class ViewController: UIViewController, PuzzleDelegate, SettingsDelegate {
       gameinfos.reset()
       self.startPuzzle()
    }
+}
+
+// MARK: - Extension Bundle
+
+extension Bundle {
+
+    var appName: String {
+        return infoDictionary?["CFBundleName"] as! String
+    }
+
+    var bundleId: String {
+        return bundleIdentifier!
+    }
+
+    var versionNumber: String {
+        return infoDictionary?["CFBundleShortVersionString"] as! String
+    }
+
+    var buildNumber: String {
+        return infoDictionary?["CFBundleVersion"] as! String
+    }
+
 }

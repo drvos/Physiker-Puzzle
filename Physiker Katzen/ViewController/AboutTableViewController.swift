6e43920a18f60c1d94a8aef51d734013bdc467e5
Volker Schering <mail@volker-schering.de>
2023-05-18 08:41:53 +0200

    Customization AboutView

--- Physiker Katzen/ViewController/AboutTableViewController.swift
+++ Physiker Katzen/ViewController/AboutTableViewController.swift
@@ -9,24 +9,13 @@ import UIKit
 
 class AboutTableViewController: UITableViewController {
    
-   @IBOutlet weak var appNameLabel: UILabel!
-   @IBOutlet weak var appVersionLabel: UILabel!
-   @IBOutlet weak var appIconImageView: UIImageView!
    @IBOutlet weak var appDeveloperLabel: UILabel!
    @IBOutlet weak var appStoreRatingButton: UIButton!
    
    override func viewDidLoad() {
       super.viewDidLoad()
-   
-      print(String(format: "Name %@", Bundle.main.appName))
-      print(String(format: "Bundle %@", Bundle.main.bundleId))
-      print(String(format: "Version %@", Bundle.main.versionNumber))
-      print(String(format: "Build %@", Bundle.main.buildNumber))
       
-      appNameLabel.text = Bundle.main.appName
-      appVersionLabel.text = String(format: "Version %@ (%@)", Bundle.main.versionNumber, Bundle.main.buildNumber)
       appDeveloperLabel.text = "Entwickelt von Volker Schering, 2023"
-      appIconImageView.image = UIImage(named: "AppIcon")
       
    }
    @IBAction func appStoreRatingTouchUpInside(_ sender: UIButton) {
@@ -38,26 +27,4 @@ class AboutTableViewController: UITableViewController {
       }
    }
    
-}
-
-// MARK: - Extension Bundle
-
-extension Bundle {
-
-    var appName: String {
-        return infoDictionary?["CFBundleName"] as! String
-    }
-
-    var bundleId: String {
-        return bundleIdentifier!
-    }
-
-    var versionNumber: String {
-        return infoDictionary?["CFBundleShortVersionString"] as! String
-    }
-
-    var buildNumber: String {
-        return infoDictionary?["CFBundleVersion"] as! String
-    }
-
 }
--- Physiker Katzen/ViewController/AboutViewController.swift
+++ Physiker Katzen/ViewController/AboutViewController.swift
@@ -9,7 +9,56 @@ import UIKit
 
 class AboutViewController: UIViewController {
    
+   @IBOutlet weak var appIconImage: UIImageView!
+   @IBOutlet weak var appNameLabel: UILabel!
+   @IBOutlet weak var appVersionLabel: UILabel!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
+      
+      print(String(format: "Name %@", Bundle.main.appName))
+      print(String(format: "Bundle %@", Bundle.main.bundleId))
+      print(String(format: "Version %@", Bundle.main.versionNumber))
+      print(String(format: "Build %@", Bundle.main.buildNumber))
+      
+      appIconImage.image = UIImage(named: "AppIcon")
+      appIconImage.layer.borderWidth = 1
+      appIconImage.layer.borderColor = UIColor.black.cgColor
+      appIconImage.layer.masksToBounds = false
+      appIconImage.layer.cornerRadius = appIconImage.frame.width/2
+      appIconImage.clipsToBounds = true
+      
+      appNameLabel.text = Bundle.main.appName
+      appVersionLabel.text = String(format: "Version %@ (%@)", Bundle.main.versionNumber, Bundle.main.buildNumber)
    }
+   
+   // MARK: - IB Actions
+   
+   @IBAction func aboutCloseTapped(_ sender: UIBarButtonItem) {
+      self.dismiss(animated: true)
+   }
+   @IBAction func aboutShareTapped(_ sender: UIBarButtonItem) {
+   }
+}
+
+// MARK: - Extension Bundle
+
+extension Bundle {
+
+    var appName: String {
+        return infoDictionary?["CFBundleName"] as! String
+    }
+
+    var bundleId: String {
+        return bundleIdentifier!
+    }
+
+    var versionNumber: String {
+        return infoDictionary?["CFBundleShortVersionString"] as! String
+    }
+
+    var buildNumber: String {
+        return infoDictionary?["CFBundleVersion"] as! String
+    }
+
 }
